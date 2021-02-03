bc9feb42300ca8e7c49ba36aac21465f145f1729
liveipooli(李为) <liveipooli@webank.com>
2021-02-03 16:41:30 +0800

    UPDATE: excel in widget should be readOnly

--- webapp/app/containers/Widget/components/Widget/index.tsx
+++ webapp/app/containers/Widget/components/Widget/index.tsx
@@ -259,7 +259,7 @@ export class Widget extends React.Component<
     }
 
     // visualis和datawrangler测试环境不一样，所以测试环境下要写死这个ip
-    const dataWranglerUrl = `http://dataWranglerServerIp:dataWranglerServerPort/#/sheet/add?simpleMode=true&showBottomBar=false&readonly=true&showChangeModeButton=false&visualisData=${JSON.stringify(visualisData)}`
+    const dataWranglerUrl = `http://dataWranglerServerIp:dataWranglerServerPort/#/sheet/add?simpleMode=true&showBottomBar=false&readOnly=true&showChangeModeButton=false&visualisData=${JSON.stringify(visualisData)}`
     return (
       <div className={styles.wrapper + ' widget-class'} ref={this.container} id="widget" style={{overflowX: 'auto', overflowY: 'hidden'}}>
         <WaterMask {...waterMaskProps} />
